56c3d1ee7056776dfe9f3dc83061195e99af6125
Koustubha068 <KoustuK@gmail.com>
2024-04-04 14:25:35 -0400

    added an enhancement of alert

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function addName() { //a function that adds  names to the array,namesArray
     const nameInput = document.getElementById('nameInput') //storing name input element as a var to get the value of the element
     const name = nameInput.value.trim() //get the TRIMMED value of the input box and store in a var which will give the name so it can be added
 
+    if (name === '') {
+        alert("Please enter a name") // the if statement says if nothing is entered in the input box, an alert is given to enter a name
+        return
+    }
+
     namesArray.push(name)//add the name to the end of the array so it can be added to the list
     displayNames() // Call the displayNames function to update the list
 
     nameInput.value = '' // Clear the input field after adding the name
+
 
 
 }
@@ -16,7 +22,7 @@ function addName() { //a function that adds  names to the array,namesArray
 function displayNames() { //function that displays the names added in the namesArray
     const nameList = document.getElementById('nameList') //storing name list element as a var
     nameList.innerHTML = ''// clear out previous list items
-    nameList.style.fontSize = '4rem'
+    nameList.style.fontSize = '4rem'// changed the size of the font of names that appear in javascipt
 
     for (let i = 0; i < namesArray.length; i++) { // loop that initializes the variable, "i" to 0, and continues to iterate as long as i is less than the length of the namesArray and increments i by 1 each time. Stops the loop when i is equal to or more than the length of the namesArray
         const name = namesArray[i] //stores the value of "nameArray" array at index i to the variable "name"
@@ -34,10 +40,10 @@ function displayNames() { //function that displays the names added in the namesA
 
 
 }
-function pickRandomName (){ // creates the function pickRandomName which will pick a random name when the button is clicked
+function pickRandomName() { // creates the function pickRandomName which will pick a random name when the button is clicked
     const randomNameDiv = document.getElementById('randomName') //stores the "randomName" element as a variable called randomNameDiv which will be used to assign the variable an empty string 
-    document.getElementById('randomName').classList = 'text-danger'
-    randomNameDiv.style.fontSize='5rem'
+    document.getElementById('randomName').classList = 'text-danger' // changed the color of the text to red (for names that appear when random button clicked)
+    randomNameDiv.style.fontSize = '5rem' // changed the size of the font of the random names that appear in javascipt
     randomNameDiv.textContent = ''//takes the variable, "randomNameDiv" and makes the text content an empty string which clears previous content
     //generate and select random name from the namesarray
     const randomNumber = Math.floor(Math.random() * namesArray.length) //creates a random number to select a random name. The random number created is stored as a var. To create a random number, a random number between 0 and 1 (Math.random) is multiplied with the length of the namesArray and then is rounded down to the nearest integer. This is stored as a variable
@@ -48,7 +54,7 @@ function pickRandomName (){ // creates the function pickRandomName which will pi
 }
 
 //Event Listener for the button click to add a name. Buttons only work with this
-document.getElementById('addNameBtn').addEventListener('click',addName)
+document.getElementById('addNameBtn').addEventListener('click', addName)
 
 
 //Event Listener for the button click to select and display a name
